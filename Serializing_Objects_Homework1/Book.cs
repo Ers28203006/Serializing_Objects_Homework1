705e38bf5dc5d00cb7309573dfc1074cb6ed205e
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-03-10 00:03:31 +0600

    Добавьте файлы проекта.

---
+++ Serializing_Objects_Homework1/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serializing_Objects_Homework1
+{
+    [Serializable]
+    class Book
+    {
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book(string name, double cost, string author, int year)
+        {
+            Name = name;
+            Cost = cost;
+            Author = author;
+            Year = year;
+        }
+
+        public void ShowBooks()
+        {
+            Console.WriteLine("Название книги: {0}", Name);
+            Console.WriteLine("Стоимость книги: {0}", Cost);
+            Console.WriteLine("Автор книги: {0}", Author);
+            Console.WriteLine("Год публикации книги: {0}", Year);
+            Console.WriteLine("***********************");
+        }
+
+    }
+}
---
+++ Serializing_Objects_Homework1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serializing_Objects_Homework1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. Объявить в консольном приложении класс Book с полями: название, стоимость, автор, год.
+             * Создать коллекцию элементов Book и заполнить тестовыми данными. 
+             * С помощью класса BinaryFormatter сохранить состояние приложения в двоичный файл.
+             * 2. На основании задания 1 восстановить состояние приложения из двоичного файла.*/
+
+            string name = "Ревизор";
+            double cost = 50000;
+            string author = "Гоголь Н.";
+            int year = 2019;
+
+            Book book1 = new Book(name, cost, author, year);
+            Book book2 = new Book(name, cost, author, year);
+            Book book3 = new Book(name, cost, author, year);
+            Book book4 = new Book(name, cost, author, year);
+
+            List<Book> books = new List<Book>() { book1, book2, book3, book4 };
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("book.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, books);
+                Console.WriteLine("Объект создан");
+            }
+
+            using (FileStream fs = new FileStream("book.dat", FileMode.OpenOrCreate))
+            {
+                List<Book> newBooks = (List<Book>)formatter.Deserialize(fs);
+
+                foreach (var book in newBooks)
+                {
+                    book.ShowBooks();
+                }
+            }
+
+        }
+    }
+}
